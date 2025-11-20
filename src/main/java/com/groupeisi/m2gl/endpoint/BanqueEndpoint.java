d57b344682be653bc9a60c6c9cc3fca06ccec473
Ngoundje <ngoundjembaye@gmail.com>
2025-11-20 19:19:45 +0000

    des modifications

--- src/main/java/com/groupeisi/m2gl/endpoint/BanqueEndpoint.java
+++ src/main/java/com/groupeisi/m2gl/endpoint/BanqueEndpoint.java
@@ -1,7 +1,17 @@
 package com.groupeisi.m2gl.endpoint;
 
+import com.groupeisi.m2gl.AddClientRequest;
+import com.groupeisi.m2gl.AddPaymentRequest;
+import com.groupeisi.m2gl.AddTransferRequest;
+import com.groupeisi.m2gl.ClientInfo;
+import com.groupeisi.m2gl.ClientResponse;
 import com.groupeisi.m2gl.GetSoldeRequest;
+import com.groupeisi.m2gl.PaymentInfo;
+import com.groupeisi.m2gl.PaymentResponse;
+import com.groupeisi.m2gl.ServiceStatus;
 import com.groupeisi.m2gl.SoldeResponse;
+import com.groupeisi.m2gl.TransferInfo;
+import com.groupeisi.m2gl.TransferResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -12,16 +22,64 @@ public class BanqueEndpoint {
 
     private static final String NAMESPACE_URI = "http://www.groupeisi.com/banque";
 
+    // ------------------- addClient -------------------
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "addClientRequest")
+    @ResponsePayload
+    public ClientResponse addClient(@RequestPayload AddClientRequest request) {
+        ClientResponse response = new ClientResponse();
+        ClientInfo client = new ClientInfo();
+        ClientInfo clientInfo = request.getClient();
+
+        ServiceStatus status = new ServiceStatus();
+        status.setStatus("SUCCESS");
+        status.setMessage("Client ajouté avec succès");
+
+        response.setStatus(status);
+        response.setClient(clientInfo);
+
+        return response;
+    }
+
+    // ------------------- getSolde -------------------
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getSoldeRequest")
     @ResponsePayload
     public SoldeResponse getSolde(@RequestPayload GetSoldeRequest request) {
         SoldeResponse response = new SoldeResponse();
-        // Ici la logique métier (exemple en dur)
-        if (request.getTel() == 123L) {  // correspond au XSD
-            response.setSolde(50000L);
-        } else {
-            response.setSolde(0L);
-        }
+        response.setSolde(100000);
+        return response;
+    }
+
+    // ------------------- addTransfer -------------------
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "addTransferRequest")
+    @ResponsePayload
+    public TransferResponse addTransfer(@RequestPayload AddTransferRequest request) {
+        TransferResponse response = new TransferResponse();
+        TransferInfo transfer = request.getTransfer();
+
+        ServiceStatus status = new ServiceStatus();
+        status.setStatus("SUCCESS");
+        status.setMessage("Transfert effectué avec succès");
+
+        response.setTransfer(transfer);
+        response.setStatus(status);
+
+        return response;
+    }
+
+    // ------------------- addPayment -------------------
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "addPaymentRequest")
+    @ResponsePayload
+    public PaymentResponse addPayment(@RequestPayload AddPaymentRequest request) {
+        PaymentResponse response = new PaymentResponse();
+        PaymentInfo payment = request.getPayment();
+
+        ServiceStatus status = new ServiceStatus();
+        status.setStatus("SUCCESS");
+        status.setMessage("Paiement effectué avec succès");
+
+        response.setPayment(payment);
+        response.setStatus(status);
+
         return response;
     }
 }
--- src/main/java/com/groupeisi/ms2/entity/package-info.java
+++ src/main/java/com/groupeisi/ms2/entity/package-info.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par Eclipse Implementation of JAXB, v3.0.2 
 // Voir https://eclipse-ee4j.github.io/jaxb-ri 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2025.11.12 à 07:46:56 PM GMT 
+// Généré le : 2025.11.20 à 06:29:53 PM GMT 
 //
 
 @jakarta.xml.bind.annotation.XmlSchema(namespace = "http://www.groupeisi.com/banque", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
